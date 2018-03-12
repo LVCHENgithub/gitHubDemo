c05f1a41a2c4853d52cd516efc9b947c37bd0ca2
c469138776 <469138776@qq.com>
2018-03-12 14:11:21 +0800

    测试1.0

---
+++ src/main/java/com/bdqn/test/Demo1.java
@@ -0,0 +1,11 @@
+package com.bdqn.test;
+
+/**
+ * Created by cai on 2018/3/12.
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("123");
+
+    }
+}
