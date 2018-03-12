066c7d12a9d555e69545a207d74f507069aca9f3
LVCHENgithub <1213218204@qq.com>
2018-03-12 16:20:22 +0800

    新版本

--- src/main/java/com/bdqn/test/Demo1.java
+++ src/main/java/com/bdqn/test/Demo1.java
@@ -7,6 +7,7 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("4567");
+        System.out.println("才踩踩踩从此");
 
     }
 }
